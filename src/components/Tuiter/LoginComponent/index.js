9110de5962c02915d8c5f22798e75422eef3dd85
Alamu Ramasamy <alamuramasamy12@gmail.com>
2022-04-24 16:58:11 -0400

    styling login signup

--- src/components/Tuiter/LoginComponent/index.js
+++ src/components/Tuiter/LoginComponent/index.js
@@ -21,25 +21,25 @@ const Login = ({usertype}) => {
     }
 
     return (
-        <div className="container col-12 col-sm-10 col-md-8 col-lg-6 col-xl-4 col-xxl-4 pt-5">
+        <div className="container col-12 col-sm-10 col-md-8 col-lg-6 col-xl-4 col-xxl-4 mt-5 p-5 wd-blueBG">
             <form>
-                <h3 className="wd-blueFont">Sign In</h3>
+                <h3 className="wd-whiteFont">Sign In</h3>
 
                 <div className="form-group">
-                    <label className="wd-blueFont wd-fontBold">Email address</label>
+                    <label className="wd-fontBold">Email address</label>
                     <input type="email" className="form-control border" placeholder="Enter email" onChange={event =>
                         setemail(event.target.value)}/>
                 </div>
 
                 <div className="form-group">
-                    <label className="wd-blueFont wd-fontBold">Password</label>
+                    <label className=" wd-fontBold">Password</label>
                     <input type="password" className="form-control border" placeholder="Enter password" onChange={event =>
                         setpassword(event.target.value)}/>
                 </div>
                 <br></br>
                 <button type="submit" className="btn btn-primary btn-block wd-fontBold" onClick={submitted}>Submit</button>
                 <br></br>
-                <label className="wd-blueFont">New User? <Link to={{pathname: `/${usertype}/register`}}>Click here to Register as new {usertype}</Link>
+                <label >New User? <Link to={{pathname: `/${usertype}/register`}}>Click here to Register as new {usertype}</Link>
                     </label>
             </form>
         </div>
--- src/components/Tuiter/SignUpComponent/index.js
+++ src/components/Tuiter/SignUpComponent/index.js
@@ -56,36 +56,36 @@ const SignUp = ({userStatus}) => {
     }
 
     return (
-        <div className="container p-5 col-12 col-sm-10 col-md-8 col-lg-6 col-xl-4 col-xxl-4">
-                <h3 className="wd-blueFont">Sign Up</h3>
+        <div className="container mt-5 p-5 col-12 col-sm-10 col-md-8 col-lg-6 col-xl-4 col-xxl-4 wd-blueBG">
+                <h3 className="wd-whiteFont">Sign Up</h3>
             <form>
                 <div className="form-group">
-                    <label className="wd-blueFont wd-fontBold">First name</label>
+                    <label className=" wd-fontBold">First name</label>
                     <input type="text" className="form-control border" placeholder="First name" onChange={event =>
                         setfirstname(event.target.value)}/>
                 </div>
 
                 <div className="form-group">
-                    <label className="wd-blueFont wd-fontBold">Last name</label>
+                    <label className=" wd-fontBold">Last name</label>
                     <input type="text" className="form-control border" placeholder="Last name" onChange={event =>
                         setlastname(event.target.value)}/>
                 </div>
 
                 <div className="form-group">
-                    <label className="wd-blueFont wd-fontBold">Email address</label>
+                    <label className=" wd-fontBold">Email address</label>
                     <input type="email" className="form-control border" placeholder="Enter email" onChange={event =>
                         setemail(event.target.value)} />
                 </div>
 
                 <div className="form-group">
-                    <label className="wd-blueFont wd-fontBold">Password</label>
+                    <label className=" wd-fontBold">Password</label>
                     <input type="password" className="form-control border" placeholder="Enter password" onChange={event =>
                         setpassword(event.target.value)}/>
                 </div>
                 <br></br>
                 <button type="submit" className="btn btn-primary btn-block" onClick={submitted}>Sign Up</button>
 
-                <p className="forgot-password text-right wd-blueFont ">
+                <p className="forgot-password text-right  ">
                     Already registered as {userStatus}? <Link to="/login">Sign in</Link>
                 </p>
             </form>
