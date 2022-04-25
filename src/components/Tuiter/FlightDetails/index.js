cc8c24691d84210cc43f6dae1d0b08d583943713
Alamu Ramasamy <alamuramasamy12@gmail.com>
2022-04-25 00:15:17 -0400

    flight details card

--- src/components/Tuiter/FlightDetails/index.js
+++ src/components/Tuiter/FlightDetails/index.js
@@ -4,10 +4,10 @@ import {Link} from "react-router-dom";
 const FlightDetails = ({flightDetails}) => {
     return(
         <>
-            <div className="container wd-fg-color-black wd-whiteBG">
+            <div className="container wd-fg-color-white  card p-5 mt-5 wd-blueBG">
                 <div className="row mt-2">
                     <div className="col-2 ps-3 me-3" style={{width:'100%'}}>
-                        <label className="wd-fg-color-blue h6 fw-bold">{flightDetails.legs[0].carriers.marketing[0].name}&nbsp;</label>
+                        <label className="wd-fg-color-white h5 fw-bold">{flightDetails.legs[0].carriers.marketing[0].name}&nbsp;</label>
                         <label className="wd-topic wd-fg-color-light-gray">&nbsp; {flightDetails.legs[0].segments[0].flightNumber}</label>
                     </div>
                 </div>
@@ -25,7 +25,7 @@ const FlightDetails = ({flightDetails}) => {
                             textRendering="geometricPrecision"
                             viewBox="0 0 500 500"
                             xmlns="http://www.w3.org/2000/svg">
-                          <g stroke="#222">
+                          <g stroke="#fff">
                             <line
                                 fill="none"
                                 strokeLinecap="round"
@@ -36,7 +36,7 @@ const FlightDetails = ({flightDetails}) => {
                                 y2="390"/>
                             <path
                                 d="M98 325c-9 10 10 16 25 6l311-156c24-17 35-25 42-50 2-15-46-11-78-7-15 1-34 10-42 16l-56 35 1-1-169-31c-14-3-24-5-37-1-10 5-18 10-27 18l122 72c4 3 5 7 1 9l-44 27-75-15c-10-2-18-4-28 0-8 4-14 9-20 15l74 63z"
-                                fill="#222"
+                                fill="#fff"
                                 strokeLinejoin="round"
                                 strokeWidth="10"/>
                           </g>
@@ -54,7 +54,7 @@ const FlightDetails = ({flightDetails}) => {
                 </div>
                 <div className= "border-top border-secondary">
                     <Link to="/login">
-                        <button className="primary rounded-pill mt-2">Book</button>
+                        <button className="btn-primary rounded-pill mt-3">Book</button>
                     </Link>
                 </div>
             </div>
